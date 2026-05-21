f403203b87972feae81dec60fc072323edcb3887
George <george.levines@gmail.com>
2026-05-21 15:22:44 -0700

    Update workflow and notebook names in get.py

--- get.py
+++ get.py
@@ -14,8 +14,8 @@ def get():
     
     logger.info('Getting notebooks from Redivis...')
     username = "glevines"  # Replace with your Redivis username
-    workflow_name = "murder-suicides-family-annihilations-gl"  # Replace with your workflow name
-    notebook_name = "murder-suicides-family-annihilations"  # Replace with your notebook name
+    workflow_name = "murder-suicides-family-annihilations-gl:j5pt"  # Replace with your workflow name
+    notebook_name = "murder-suicides-family-annihilations:15s0"  # Replace with your notebook name
     
     notebook = redivis.notebook(f"{username}.{workflow_name}.{notebook_name}")
     
